83ffb35eb6ba901c844f3531b2695ddc6a276531
mayu itakura <itakura-mayu-cv@ynu.jp>
2025-09-22 11:21:12 +0900

    ^x ^

---
+++ pages/high_and_low2.py
@@ -0,0 +1,11 @@
+import json
+import streamlit as st
+from pathlib import Path
+
+# ----Pathを指定して JSONファイルを読み込み ----
+json_path = Path(__file__).parent.parent / "sample_data" / "highlow_round3.json"
+with open(json_path, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+st.session_state.deck = data["deck"]
+st.write(data)
